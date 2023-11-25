c1dd8c52ee80ddc27dfb67e23f463a7b4f2b2333
Kiran Kumar.B <151979719+kirancse9@users.noreply.github.com>
2023-11-25 12:28:13 +0530

    Add files via upload

--- JavaScript/ReverseWord.js
+++ JavaScript/ReverseWord.js
@@ -1,1 +1,18 @@
+function reverseWordsInSentence(sentence) {
+    const words = sentence.split(/\s+/);
 
+    const reversedWords = words.map(reverseString);
+
+    return reversedWords.join(' '); 
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join(''); 
+}
+
+
+const inputSentence = "This is a sunny day";
+const reversedSentence = reverseWordsInSentence(inputSentence);
+
+console.log("Original sentence: " + inputSentence);
+console.log("Reversed sentence: " + reversedSentence);
